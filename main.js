e66e6fca8be2b7a9108895bd968b0e7ad66470fb
Kris J <KrisJohnson458@gmail.com>
2018-06-24 11:39:09 -0500

    Zong's prototype files

---
+++ main.js
@@ -0,0 +1,66 @@
+//Initial game vars
+var level = 1;
+var money = 0;
+var clickDamage = 10;
+var passiveDamage = 0;
+var clickUpgradePrice = 10;
+var passiveUpgradePrice = 10;
+var monsterHealth = 10;
+
+//Monster takes given damage, sets monsterHealth to 0 if negative, and then updates the game
+function damageMonster(damageToTake) {
+    monsterHealth -= damageToTake;
+
+    if (monsterHealth < 0) monsterHealth = 0;
+
+    updateMonsterAndLevel();
+}
+
+//Increments level if monster is "dead", and then updates the screen for monsterhealth, and level (if needed)
+function updateMonsterAndLevel() {
+    if (monsterHealth == 0) {
+        level++;
+        money += 100;
+        monsterHealth = generateMonster(level);
+
+        document.getElementById("level").innerHTML = level;
+        document.getElementById("money").innerHTML = money;
+    }
+    document.getElementById("monsterHealth").innerHTML = monsterHealth;
+}
+
+//New monsters have health based on the level
+function generateMonster(level) {
+    return 10 * level ** 2;
+}
+
+//When the page is fully loaded, passive damage will start being done to the monster (if passiveDamage > 0)
+window.onload = function () {
+    setInterval(doPassiveDamage, 1000);
+}
+
+/*This is a helperfunction that allows setInterval (see above) to properly apply passive damage every
+1000 milliseconds. Refer to link below to find justification:
+https://stackoverflow.com/questions/10182714/why-does-the-setinterval-callback-execute-only-once */
+
+function doPassiveDamage() {
+    damageMonster(passiveDamage);
+}
+
+function upgradeClick() {
+    if (money >= 10) {
+        money -= 10;
+        clickDamage += 5;
+        document.getElementById("money").innerHTML = money;
+        document.getElementById("clickDamage").innerHTML = clickDamage;
+    }
+}
+
+function upgradePassive() {
+    if (money >= 100) {
+        money -= 100;
+        passiveDamage += 10;
+        document.getElementById("money").innerHTML = money;
+        document.getElementById("passiveDamage").innerHTML = passiveDamage;
+    }
+}
